48548cb204308f744920095bc88da532702901cc
Andre Rosa <andre_rosa@hms.harvard.edu>
2018-03-11 18:14:38 -0400

    DI-1078
    changed name of networks to make it more obvious.
    
    encrypted networks will only be used for in-house deployment only. will
    make a note about that option in the wiki.
    
    fixed a couple bugs

--- deployments/tools/switchenv.sh
+++ deployments/tools/switchenv.sh
@@ -104,7 +104,7 @@ if [ -z "${db_host}" ]; then
     return 1
 fi
 
-if [[ $(docker ps --format "{{.Names}} {{.Ports}}" | grep ${COMPOSE_PROJECT_NAME} | grep "\->") ]]; then
+if [[ $(docker ps --format "{{.Names}} {{.Ports}}" | grep -s ${COMPOSE_PROJECT_NAME} | grep "\->") ]]; then
     echo "WARNING: Previous project has docker host bound ports still up."
     echo "WARNING: There may be possible port conflicts."
     eval 'docker ps --format "{{.Names}} {{.Ports}}" | grep ${COMPOSE_PROJECT_NAME} | grep "\->"'
