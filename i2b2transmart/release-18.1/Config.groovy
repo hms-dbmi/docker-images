68fbc62021323a59d5f261d25308618c57adeecf
Andre Rosa <andre_rosa@hms.harvard.edu>
2018-04-24 19:15:15 -0400

    i2b2tm-15 / i2b2tm-22 / secrets management
    1. now using mysql version 5.7.22 instead of latest
    2. add rserve to prod and dev stacks
    3. organized Config.groovy, added comments, added Rserve and SOLR
    configs
    4. temporary solr fix allowing for invalid characters in request
    5. added secrets management build options for i2b2transmart and pic-sure

--- i2b2transmart/release-18.1/Config.groovy
+++ i2b2transmart/release-18.1/Config.groovy
@@ -5,6 +5,8 @@ import org.transmart.searchapp.Role
 grails {
 	plugin {
 		springsecurity {
+
+			/* {{{ Auth0 configuration */
 			auth0 {
 				active = "${System.getenv("AUTH0_ACTIVE")}"
 				clientId = "${System.getenv("AUTH0_CLIENT_ID")}"
@@ -13,6 +15,8 @@ grails {
 				useRecaptcha = false
 				webtaskBaseUrl = "${System.getenv("AUTH0_WEBTASK_URL")}"
 			}
+			/* }}} */
+
 			apf.storeLastUsername = true
 			authority.className = Role.name
 			controllerAnnotations.staticRules = [
@@ -52,12 +56,13 @@ grails {
 	}
 }
 
-
-edu.harvard.transmart.email.notify = "${System.getenv("NOTIFICATION_EMAILS")}"
-edu.harvard.transmart.email.logo = '/images/info_security_logo_rgb.png'
+/* {{{ UI Configuration */
 com.recomdata.searchtool.largeLogo = 'transmartlogoHMS.jpg'
 com.recomdata.searchtool.appTitle = 'Department of Biomedical Informatics – tranSMART'
+/* }}} */
 
+
+/* {{{ Logging Configuration */
 grails.logging.jul.usebridge = true
 
 log4j = {
@@ -78,6 +83,102 @@ log4j = {
 		warn 'file'
 	}
 }
+/* }}} */
+
+
+/* {{{ Faceted Search Configuration */
+/* {{{ File store and indexing configuration */
+com.rwg.solr.scheme = 'http'
+com.rwg.solr.host   = 'solr:' + 8983
+com.rwg.solr.path   = '/solr/rwg/select/'
+com.rwg.solr.browse.path   = '/solr/browse/select/'
+com.rwg.solr.update.path = '/solr/browse/dataimport/'
+com.recomdata.solr.baseURL = "${com.rwg.solr.scheme}://${com.rwg.solr.host}" +
+                            "${new File(com.rwg.solr.browse.path).parent}"
+
+com {
+	recomdata {
+		solr {
+			maxNewsStories = 10
+			maxRows = 10000
+		}
+	}
+}
+/* }}} */
+
+/* {{{ Sample Explorer configuration */
+
+// This is an object to dictate the names and 'pretty names' of the SOLR fields.
+// Optionally you can set the width of each of the columns when rendered
+
+// TODO: this is configured twice in transmart-data:release-16.2. Is it a bug? -Andre
+sampleExplorer {
+    fieldMapping = [
+        columns:[
+            [header:'ID', dataIndex:'id', mainTerm: true, showInGrid: true, width:20],
+            [header:'trial name', dataIndex:'trial_name', mainTerm: true, showInGrid: true, width:20],
+            [header:'barcode', dataIndex:'barcode', mainTerm: true, showInGrid: true, width:20],
+            [header:'plate id', dataIndex:'plate_id', mainTerm: true, showInGrid: true, width:20],
+            [header:'patient id', dataIndex:'patient_id', mainTerm: true, showInGrid: true, width:20],
+            [header:'external id', dataIndex:'external_id', mainTerm: true, showInGrid: true, width:20],
+            [header:'aliquot id', dataIndex:'aliquot_id', mainTerm: true, showInGrid: true, width:20],
+            [header:'visit', dataIndex:'visit', mainTerm: true, showInGrid: true, width:20],
+            [header:'sample type', dataIndex:'sample_type', mainTerm: true, showInGrid: true, width:20],
+            [header:'description', dataIndex:'description', mainTerm: true, showInGrid: true, width:20],
+            [header:'comment', dataIndex:'comment', mainTerm: true, showInGrid: true, width:20],
+            [header:'location', dataIndex:'location', mainTerm: true, showInGrid: true, width:20],
+            [header:'organism', dataIndex:'source_organism', mainTerm: true, showInGrid: true, width:20],
+			// consolidated the sampleExplorer field mapping. is this a bug? -Andre
+			[header:'Sample ID',dataIndex:'id', mainTerm: false, showInGrid: false],
+			[header:'BioBank', dataIndex:'BioBank', mainTerm: true, showInGrid: true, width:10],
+			[header:'Source Organism', dataIndex:'Source_Organism', mainTerm: true, showInGrid: true, width:10]
+			// Continue as you have fields
+
+		]
+    ]
+    resultsGridHeight = 100
+    resultsGridWidth = 100
+    idfield = 'id'
+}
+
+// TODO: this is configured twice in transmart-data:release-16.2. Is it a bug? -Andre
+edu.harvard.transmart.sampleBreakdownMap = [
+    "aliquot_id":"Aliquots in Cohort",
+	"id":"Aliquots in Cohort"
+
+]
+/* }}} */
+
+
+/* {{{ Rserve configuration */
+// This is to target a remove Rserve. Bear in mind the need for shared network storage
+def jobsDirectory     = "/var/tmp/jobs/"
+
+RModules.host = "rserve"
+RModules.port = 6311
+
+// This is not used in recent versions; the URL is always /analysisFiles/
+RModules.imageURL = "/tempImages/" //must end and start with /
+
+production {
+    // The working directory for R scripts, where the jobs get created and
+    // output files get generated
+    RModules.tempFolderDirectory = jobsDirectory
+}
+development {
+    RModules.tempFolderDirectory = "/tmp"
+
+    /* we don't need to specify temporaryImageDirectory, because we're not copying */
+}
+
+// Used to access R jobs parent directory outside RModules (e.g. data export)
+com.recomdata.plugins.tempFolderDirectory = RModules.tempFolderDirectory
+/* }}} */
+
+
+/* {{{ Email notification configuration */
+edu.harvard.transmart.email.notify = "${System.getenv("NOTIFICATION_EMAILS")}"
+edu.harvard.transmart.email.logo = '/images/info_security_logo_rgb.png'
 
 grails {
 	mail {
@@ -93,28 +194,10 @@ grails {
 		         'mail.smtp.socketFactory.fallback': 'false']
 	}
 }
+/* }}} */
 
-    // This is to target a remove Rserv. Bear in mind the need for shared network storage
-    RModules.host = "rserve"
-    RModules.port = 6311
 
-    // This is not used in recent versions; the URL is always /analysisFiles/
-    RModules.imageURL = "/tempImages/" //must end and start with /
-
-    production {
-        // The working directory for R scripts, where the jobs get created and
-        // output files get generated
-        RModules.tempFolderDirectory = jobsDirectory
-    }
-    development {
-        RModules.tempFolderDirectory = "/tmp"
-
-        /* we don't need to specify temporaryImageDirectory, because we're not copying */
-    }
-
-    // Used to access R jobs parent directory outside RModules (e.g. data export)
-    com.recomdata.plugins.tempFolderDirectory = RModules.tempFolderDirectory
-
+/* {{{ Fractalis configuration */
 fractalis {
 	active = "${System.getenv("FRACTALIS_ACTIVE")}"
 	// Must be a PIC-SURE endpoint unless i2b2-tranSMART supports additional data APIs.
@@ -123,8 +206,7 @@ fractalis {
 	resourceName = "${System.getenv("FRACTALIS_RESOURCE_NAME")}"
 	// Must be a Fractalis endpoint. See https://git-r3lab.uni.lu/Fractalis for further information.
 	node = "${System.getenv("FRACTALIS_NODE")}"
-
-
 }
+/* }}} */
 
 org.transmart.configFine = true
