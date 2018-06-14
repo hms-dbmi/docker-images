7f490ef23650a9972e5bd249d0d2e83388e97ae1
Andre Rosa <andre_rosa@hms.harvard.edu>
2018-06-14 14:55:33 -0400

    bug fixes. made secret_getter as small as possible BIOMART_USER -> DB_PASSWORD no longer ignoring .env files. They will be treated as default meta values for deployment

--- deployments/tools/switchenv.sh
+++ deployments/tools/switchenv.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 #POSIX
-
 usage() {
     echo "Usage:"
     echo "  source switchenv.sh [ARGS]"
     echo "  source switchenv.sh -h|--help"
     echo ""
     echo "Enviornment Arguments:"
-    echo "  -e, --environment ENV           [required] Project to deploy."
-    echo "                                  [prerequisite] Matching <ENV>.env file in current directory."
-    echo ""
-    echo "  -t, --type transmart|irct       [required] Deployment types. Determines Database type."
-    echo ""
-    echo ""
-    echo "  -r, --remote true|false         Enables ssh-tunneling for remote database use [default: false]."
-    echo "                                  [prerequisite] ssh-agent."
-    echo "                                  [prerequisite] Matching ssh config option <ENV> in /.ssh/config."
+    echo "  -e, --environment ENV       [required] Project to deploy."
+    echo "                              [prerequisite] Matching <ENV>.env, <ENV>.secret files in current directory."
+    echo "  -t, --type transmart|irct   [required] Deployment types. Determines Database type."
+    echo "  -r, --remote true|false     Enables ssh-tunneling for remote database use [default: false]."
+    echo "                              [prerequisite] ssh-agent."
+    echo "                              [prerequisite] Matching ssh config option <ENV> in /.ssh/config."
     echo ""
     echo "Secrets Arguments:"
     echo "  -s, --secrets vault|file|none   Secrets Vault to use [default: none]"
-    echo ""
     echo "  --path PATH                     PATH to Vault token (vault), PATH to encrypted secrets (file)"
+    echo ""
+    echo "  --options OPTIONS...            OPTIONS available for secret_getter"
+    echo "  --options help                 usage for secret_getter"
+    echo "  --more MORE,...                  MORE options to be exported as environment variables"
     echo ""
     echo "Service Versioning Arguments:"
     echo ""
@@ -58,13 +57,21 @@ param() {
     esac
 }
 
+# environment
 env=
-type="transmart"
+# deployment type (should get deprecated one day)
+type=
+# ssh tunnel remote
 remote="false"
+# secrets
 secrets=
-path=
+# secrets options
+options=
+# service
 service=
+# version
 version=
+# dry run
 confirm="false"
 
 while :; do
@@ -82,8 +89,8 @@ while :; do
         -s|--secrets)
             secrets=$(param $1 $2 "-s" "--secrets")
             ;;
-        -p|--path)
-            path=$(param $1 $2 "-p" "--path")
+        --options)
+            options=$(param $1 $2 "--options" "--options")
             ;;
         -r|--remote)
             remote=$(param $1 $2 "-r" "--remote")
@@ -114,18 +121,43 @@ while :; do
     shift
 done
 
-if [ -z "${env}" ] || [ -z "${type}" ]; then
+### Service Versioning ####
+# both service and version need to be set
+if { [ "x${service}" != "x" ] && [ "x${version}" != "x" ]; }; then
+    echo "# service version"
+    echo "${service}_version=${version}"
+    echo ""
+    if [ "${confirm}" != "true" ]; then
+        export ${service}_version=${version}
+    fi
+    # we only care about changing service versions. Forget the rest of the script
+    # if env and type values are not set, exit out - Andre
+    if [ -z "${env}" ] || [ -z "${type}" ]; then
+        return 0
+    else
+        usage "ERROR: required: --environment, --type"
+        return 1
+    fi
+# if only one is set
+elif [ "x${service}" != "x" ] || [ "x${version}" != "x" ]; then
+    usage "ERROR: Both --service and --version must be provided"
+    return 1
+elif [ -z "${env}" ] || [ -z "${type}" ]; then
     usage "ERROR: required: --environment, --type"
     return 1
 fi
+#### /Service Versioning ####
 
+
+# check if we can connect to Docker
+# TODO: docker-machine assumption no longer applicable
 if [ -z "${DOCKER_HOST}" ]; then
     echo "Connecting to Docker $(docker-machine env default)"
+
     eval $(docker-machine env default)
 fi
 
-unset DOCKER_CONTENT_TRUST
-
+# check if we need to create an ssh forward agent
 if [ "${remote}" == "true" ]; then
     echo "Forward ssh-agent to docker-machine"
     echo ""
@@ -147,6 +179,7 @@ if [ "${remote}" == "true" ]; then
     fi
 fi
 
+# does env file exist?
 echo "Using ${env}.env"
 echo ""
 if [ ! -f "${env}.env" ]; then
@@ -155,6 +188,18 @@ if [ ! -f "${env}.env" ]; then
     return 1
 fi
 
+# does secrets file exist?
+echo "Using ${env}.secret"
+echo ""
+if [ ! -f "${env}.secret" ]; then
+    echo "ERROR: ${env}.secret does not exist"
+    usage
+    return 1
+fi
+
+### Database Setup (for development)
+# We export the DB variable. Should only export with development
+# Who cares maybe?
 echo "Setting up ${type}"
 echo ""
 regex=""
@@ -193,7 +238,7 @@ if [ -z "${db_host}" ]; then
     return 1
 fi
 
-
+# exposed ports warning
 if [[ $(docker ps --format "{{.Names}} {{.Ports}}" | grep -s "\->") ]]; then
     echo "***** WARNING *****"
     echo "Docker host bound ports still up."
@@ -203,6 +248,7 @@ if [[ $(docker ps --format "{{.Names}} {{.Ports}}" | grep -s "\->") ]]; then
     echo ""
 fi
 
+#### Environment ####
 echo "# Environment Variables"
 if [ "${confirm}" == "true" ]; then
     echo "**** DRY RUN ONLY ***"
@@ -211,49 +257,58 @@ fi
 echo ""
 echo "COMPOSE_PROJECT_NAME=${env}"
 echo "ENV_FILE=${env}.env"
+echo "SECRET_FILE=${env}.secret"
 echo "SSH_CONFIG_CONFIG=${env}"
 echo "STACK_NAME=${env}"
 echo ""
 if [ "${confirm}" != "true" ]; then
     export COMPOSE_PROJECT_NAME=$env
     export ENV_FILE=$env.env
+    export SECRET_FILE=$env.secret
     export SSH_CONFIG_CONFIG=$env
     export STACK_NAME=$env
 fi
+### /Environment ####
 
+#### Database #####
 echo "# database"
 echo "${db_var}=${db_host}"
 echo ""
 export $db_var=$db_host
+#### /Database ####
 
-if [ "x${secrets}" != "x" ] && [ "x${path}" != "x"]; then
+#### Secrets ####
+if [ "${secrets}" == "none" ]; then
+    unset SG_COMMAND
+    unset SG_OPTIONS
+    return 0
+fi
+
+if [ "x${secrets}" != "x" ] && [ "x${options}" != "x" ]; then
+
+    # help?
+    if [ "${options}" == "help" ]; then
+        echo "***** SECRET-GETTER HELP *****"
+        eval "docker run --rm dbmi/secret-getter:latest"
+        echo "***** SECRET-GETTER HELP *****"
+        echo ""
+    fi
+
     echo "# secrets"
     echo "SG_COMMAND=${secrets}"
-    echo "SG_OPTIONS=${path}"
+    echo "SG_OPTIONS=${options}"
     echo ""
     if [ "${confirm}" != "true" ]; then
         export SG_COMMAND=${secrets}
-        export SG_OPTIONS=${path}
+        export SG_OPTIONS=${options}
     fi
-else
-    echo "ERROR: Both --secrets and --path must be provided"
+# if only one is set
+elif [ "x${secrets}" != "x" ] || [ "x${options}" != "x" ]; then
+    echo "ERROR: Both --secrets and --options must be provided"
     usage
     return 1
 fi
-
-if [ "x${service}" != "x" ] && [ "x${version}" != "x" ]; then
-    echo "# service version"
-    echo "${service}_version=${version}"
-    echo ""
-    if [ "${confirm}" != "true" ]; then
-        export ${service}_version=${version}
-    fi
-else
-    echo "ERROR: Both --service and --version must be provided"
-    usage
-    return 1
-fi
-
+### /Secrets ####
 
 
 return 0
